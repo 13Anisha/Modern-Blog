30af35816750e3b4f3dd2364bd99b4718d95f319
Anisha Plawat <anishaplawat13@gmail.com>
2024-04-09 19:59:39 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
+import { configDotenv } from 'dotenv';
 import express from  'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Mongodb connected");
+})
+.catch((err)=>{
+    console.log(err);
+})
 const app = express();
 
 app.listen(3000,()=>{
