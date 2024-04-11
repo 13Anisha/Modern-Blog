5172304e1c7192fe05cc9c2b83911518504e0e4d
Anisha Plawat <anishaplawat13@gmail.com>
2024-04-12 00:22:57 +0530

    latest update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
